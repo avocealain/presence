2ea97f263dadc403add09062d6213336adb07df7
Alain Avoce <avocealain1@gmail.com>
2026-05-04 17:04:31 +0300

    Fix: add missing Log facade import to TeacherController

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -9,6 +9,7 @@ use App\Models\QRSession;
 use App\Services\QRCodeService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
 
